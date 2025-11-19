6ba7bc68442f0d5f93eee9cf4fd0a4bacd3cf3aa
Gaya <143161293+YadavGaya031@users.noreply.github.com>
2025-11-19 23:33:12 +0530

    Update auth.service.js

--- Backend/src/services/auth.service.js
+++ Backend/src/services/auth.service.js
@@ -1,7 +1,7 @@
 import { createUser, findUserByEmail, findUserByEmailByPassword } from "../dao/user.dao.js"
 import { ConflictError } from "../utils/errorHandler.js"
 import {signToken} from "../utils/helper.js"
-import { sendEmail } from "../utils/sendEmail.js"
+// import { sendEmail } from "../utils/sendEmail.js"
 import User from "../models/user.model.js"
 
 export const registerUser = async (name, email, password) => {
@@ -9,13 +9,13 @@ export const registerUser = async (name, email, password) => {
     if(user) throw new ConflictError("User already exists")
     const newUser = await createUser(name, email, password)
     
-    sendEmail(
-        newUser.email,
-        "Welcome to Linkly 🎉",
-        `<h2>Hey ${newUser.name},</h2>
-        <p>Your account has been created successfully.</p>
-        <p>Start shortening URLs now!</p>`
-    );
+    // sendEmail(
+    //     newUser.email,
+    //     "Welcome to Linkly 🎉",
+    //     `<h2>Hey ${newUser.name},</h2>
+    //     <p>Your account has been created successfully.</p>
+    //     <p>Start shortening URLs now!</p>`
+    // );
 
     const token = await signToken({id: newUser._id, role: newUser.role})
     return {token,user: newUser}
