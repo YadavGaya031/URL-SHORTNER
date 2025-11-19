db0faf4a8e3d357a0575c79c8ac2b4024578346c
Gaya <143161293+YadavGaya031@users.noreply.github.com>
2025-11-19 23:32:42 +0530

    Disable sendEmail function in sendEmail.js
    Comment out the sendEmail function and its dependencies.

--- Backend/src/utils/sendEmail.js
+++ Backend/src/utils/sendEmail.js
@@ -1,17 +1,17 @@
-import { Resend } from "resend";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendEmail = async (to, subject, html) => {
-  try {
-    await resend.emails.send({
-      from: process.env.EMAIL_FROM, // Ex: Linkly <no-reply@linkly.com>
-      to,
-      subject,
-      html,
-    });
-    console.log("✅ Email sent to", to);
-  } catch (error) {
-    console.error("❌ Error sending email:", error);
-  }
-};
+// export const sendEmail = async (to, subject, html) => {
+//   try {
+//     await resend.emails.send({
+//       from: process.env.EMAIL_FROM, // Ex: Linkly <no-reply@linkly.com>
+//       to,
+//       subject,
+//       html,
+//     });
+//     console.log("✅ Email sent to", to);
+//   } catch (error) {
+//     console.error("❌ Error sending email:", error);
+//   }
+// };
